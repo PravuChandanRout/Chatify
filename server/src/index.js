e667066b50759d54b554b3229876b227f4ae7e2e
Pravu chandan Rout <rout01828@gmail.com>
2024-12-26 17:51:16 +0530

    Login & Logout & Update Profile API added

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
-  connectDB()
+  connectDB();
 });
