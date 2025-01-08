ec38df16b3143c46c9e3a453d9fab212214c13ae
Pravu chandan Rout <rout01828@gmail.com>
2025-01-08 21:07:46 +0530

    changes for production

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -14,7 +13,6 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,14 +26,6 @@ app.use(bodyParser.urlencoded({extended:true, parameterLimit:100000,limit:"10mb"
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if(process.env.NODE_ENV==="production") {
-  app.use(express.static(path.join(__dirname,"../client/dist")));
-
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
-  })
-}
 
 server.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
