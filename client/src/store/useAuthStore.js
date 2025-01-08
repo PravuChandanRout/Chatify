483b69c8723212ec0d529436b92226e5a2d80dd9
PravuChandanRout <rout01828@gmail.com>
2025-01-08 21:20:23 +0530

    Update useAuthStore.js

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const backendURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const backendURL = "https://chatify-backend-60ft.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
